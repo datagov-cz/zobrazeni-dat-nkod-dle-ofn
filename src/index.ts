85b42d70ab0dab6844cc92c20af6b46f257f810a
Jakub Straka <Jakub.Straka2@nakit.cz>
2021-01-27 13:21:09 +0100

    Přidání prvního testu

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,13 @@ import $ from "jquery";
 import {browserApp} from "./browser"
 import {demoApp} from "./demo";
 
-// TODO zvážit rozdělení na ES6 moduly, toto jsou takové POJO moduly, které se transpilují do "defaul" modulu
+// TODO zvážit rozdělení na ES6 moduly, toto jsou takové POJO moduly, které se transpilují do "default" modulu
 $(() => {
-    (() => {
+    if ($("#browser").length) {
+        void browserApp();
+    }
 
-        if ($("#browser").length) {
-            void browserApp();
-        }
-
-        if ($("#demo").length) {
-            void demoApp("app", "theTable");
-        }
-
-    })();
+    if ($("#demo").length) {
+        void demoApp("app", "theTable");
+    }
 })
