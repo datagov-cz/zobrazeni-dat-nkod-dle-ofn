04ec0c57deffbc59c2f583188df8247b13b3295c
strakajakub <22910515+strakajakub@users.noreply.github.com>
2021-02-05 07:15:26 +0000

    Deploying to gh-pages from  @ cfc32bcf7d61b8c5f36b59df0fee7903ea3bd8d8 🚀

--- demo.d.ts
+++ demo.d.ts
@@ -1,2 +1,7 @@
 import "datatables.net-bs4";
+/**
+ * Vykreslí obsah demo aplikace do elementu se zvoleným id
+ *
+ * @param elementId id elementu, do kterého se má vykreslit obsah
+ */
 export declare function renderDemoApp(elementId: string): Promise<void>;
